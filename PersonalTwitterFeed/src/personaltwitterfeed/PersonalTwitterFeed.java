16953b05e121157dea53f81be73636d53ec4d4fc
Chris Cannon <chris.c.cannon@gmail.com>
2018-09-10 10:08:54 -0400

    Greet user to twitter feed

--- PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
+++ PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
@@ -5,17 +5,35 @@
  */
 package personaltwitterfeed;
 
+import java.util.Scanner;
+
 /**
  *
  * @author CCannon
  */
 public class PersonalTwitterFeed {
 
+    private static int MAX_NUMBER_TWEETS = 200;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String[] tweets = new String[MAX_NUMBER_TWEETS];
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Welcome to your personal Twitter!");
+        System.out.println("What's your name, tweeter?");
+        
+        String tweeterName = keyboard.nextLine();
+        
+        System.out.println("Nice to meet you " + tweeterName + "!");
+        System.out.println("Enter your tweets and I will add them to your timeline!");
+        
+        int numTweets = 0;
+        
+        while(numTweets < (MAX_NUMBER_TWEETS - 1)) {
+            
+        }
     }
     
 }

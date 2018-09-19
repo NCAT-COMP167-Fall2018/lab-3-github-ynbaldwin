82cb62122de4d75476db57cfb75549701eef2d41
Yakia Baldwin <yakianycole@gmail.com>
2018-09-19 15:35:39 -0400

    Feature-2 DONEgit add .

--- PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
+++ PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
@@ -16,7 +16,8 @@ import java.util.Scanner;
 public class PersonalTwitterFeed {
 
     private static int MAX_NUMBER_TWEETS = 200;
-    
+    static int numTweets = 0;
+    static Scanner keyboard = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
@@ -25,7 +26,7 @@ public class PersonalTwitterFeed {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Welcome to your personal Twitter!");
         System.out.println("What's your name, tweeter?");
-        System.out.println(getCurrentTimeStamp());
+        
         String tweeterName = keyboard.nextLine();
         
         
@@ -33,12 +34,11 @@ public class PersonalTwitterFeed {
 
         System.out.println("Nice to meet you " + tweeterName + "!");
         System.out.println("Enter your tweets and I will add them to your timeline!");
+       // System.out.println(getCurrentTimeStamp());
         
-        int numTweets = 0;
         
-        while(numTweets < (MAX_NUMBER_TWEETS - 1)) {
-            tweets[numTweets] = keyboard.nextLine();
-            numTweets++;
+         while(numTweets < (MAX_NUMBER_TWEETS - 1)) {
+            newTweet(tweets);
             
             System.out.println(tweeterName + "'s Personal Twitter Feed:");
             for(int i = 0; i < numTweets; i++) {
@@ -63,4 +63,9 @@ public class PersonalTwitterFeed {
         String date = simpleDateFormat.format(new Date());
         return date;
     }
+    public static void newTweet(String[] tweets){
+        
+        tweets[numTweets] = keyboard.nextLine()+"\n"+ getCurrentTimeStamp();
+            numTweets++; 
+    } 
 }
